156219d3b8bccadcaa96b68d2bf35656dbde6bca
Karrllas <karrllas@gmail.com>
2022-02-06 20:26:44 +0200

    Added basic functions

--- HaskellTrain/src/Lib.hs
+++ HaskellTrain/src/Lib.hs
@@ -3,4 +3,21 @@ module Lib
     ) where
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = putStrLn "Hello"
+
+double x = x + x
+quadruple x = double (double x)
+
+factorial n = product [1 .. n]
+
+average ns = sum ns `div` length ns
+
+poly x = let
+    y = x+1
+    in y * y
+
+sumEvenOdds xs = sums (incr (evens xs))
+    where
+        sums xs = foldr (+) 0 xs
+        incr xs = map (+1) xs
+        evens xs = filter (\x -> x `mod` 2==0) xs
