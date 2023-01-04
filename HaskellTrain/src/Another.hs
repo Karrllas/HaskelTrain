1aae2e73c030597dab484a129879382133765a07
Karrllas <52286696+Karrllas@users.noreply.github.com>
2023-01-05 00:25:56 +0200

    Day 14

---
+++ HaskellTrain/src/Another.hs
@@ -0,0 +1,22 @@
+module Another where
+
+import Control.Applicative
+
+hurr :: (Num a) => a -> a
+hurr = (*2)
+
+durr :: (Num a) => a -> a
+durr = (+10)
+
+m :: (Num a) => a -> a
+m = hurr.durr
+
+m' :: (Num a) => a -> a
+m' = fmap hurr durr
+
+m2 :: Integer -> Integer
+m2 = (+) <$> hurr <*> durr
+
+m3 :: Integer -> Integer
+m3 = liftA2 (+) hurr durr
+
